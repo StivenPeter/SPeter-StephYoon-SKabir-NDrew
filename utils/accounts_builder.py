2b723596a8113dfc40383ba9416e032308351209
skabir8 <skabir@stuy.edu>
2016-11-03 03:50:12 -0400

    Fixed issues with account_builder so it could be compatible with app.py

--- utils/accounts_builder.py
+++ utils/accounts_builder.py
@@ -1,9 +1,7 @@
 import sqlite3
 import hashlib
 
-f = "../data/data.db"
-db = sqlite3.connect(f)
-c = db.cursor()
+
 
 def createAccountsTable():
 	q = "CREATE TABLE accounts (userid TEXT, pass TEXT)"
@@ -16,29 +14,40 @@ def close():
 	db.close()
 
 def addAccount(user, password):
+        f = "data/data.db"
+        db = sqlite3.connect(f)
+        c = db.cursor()
 	check = "SELECT userid FROM accounts WHERE accounts.userid = \'" + user + "\'"
 	c.execute(check)
 	if not c.fetchone(): # if userid does not exist
 		passw = hashlib.sha256(password).hexdigest()
 		q = "INSERT INTO accounts VALUES (\'%s\',\'%s\')" % (user, passw)
 		c.execute(q)
+		db.commit()
+		db.close()
 		return True
 	else: # userid exists
+                save()
+                close()
 		return False
 
 def getAccountPass(user):
+        f = "data/data.db"
+        db = sqlite3.connect(f)
+        c = db.cursor()
 	q = "SELECT pass FROM accounts WHERE accounts.userid = \'" + user + "\'"
 	results = c.execute(q);
 	for entry in results:
 		return entry[0]
+	db.commit()
+	db.close()
 		
-'''
-def test():
-	#createAccountsTable()
-	print(addAccount("a", "a"))
-	save()
-	print(getAccountPass("a"))
-	close()
 
-test()
-'''
+'''def test():
+	#createAccountsTable()
+	print(addAccount("aaaaa", "aaaaa"))
+	save()
+	print(getAccountPass("aaaaa"))
+	close()'''
+
+
